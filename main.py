48cfdeb72ea4a6d4b51dfd6092fae6a1cc2eaf0a
Himanshu <your_email@example.com>
2025-03-14 11:52:13 +0530

    `Update uvicorn.run call to use main:app instead of app:app`

--- main.py
+++ main.py
@@ -87,4 +87,4 @@ async def search_song(q: str):
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run("app:app", host="0.0.0.0", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
