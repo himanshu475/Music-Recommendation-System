ea5927c2a426aa53d3adb5497d329719c32b09e3
Himanshu <your_email@example.com>
2025-03-15 12:31:27 +0530

    "Removed trailing slash from Vercel app URL in CORS configuration."

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://127.0.0.1:5500",   # For local testing
-        "https://music-recommendation-system-zeta.vercel.app/",  
+        "https://music-recommendation-system-zeta.vercel.app",  
         "https://web-production-6fde.up.railway.app",
     ],
     allow_credentials=True,
