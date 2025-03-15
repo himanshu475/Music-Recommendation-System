336152335d39df75742a001456d41d521bba917c
Himanshu <your_email@example.com>
2025-03-15 16:52:22 +0530

    `Added two new URLs to the list of allowed origins in main.py`

--- main.py
+++ main.py
@@ -24,6 +24,8 @@ app.add_middleware(
         "http://127.0.0.1:5500",   # For local testing
         "https://music-recommendation-system-zeta.vercel.app",  
         "https://web-production-6fde.up.railway.app",
+        "https://music-recommendati-git-504dc0-himanshusali57-gmailcoms-projects.vercel.app",
+        
     ],
     allow_credentials=True,
     allow_methods=["*"],  
